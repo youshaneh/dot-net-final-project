ff619d6c4b0e872a2914c1daea4c40dc808de222
You-Sheng <youshaneh@gmail.com>
2021-06-30 16:40:14 +0800

    first commit

---
+++ FinalProject/Program.cs
@@ -0,0 +1,463 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Renderer
+    {
+        public const String paddingLeft = "   ";
+        List<Component> components;
+
+        public Renderer clear()
+        {
+            Console.Clear();
+            components = new List<Component>();
+            return this;
+        }
+
+        public Renderer addComponent(Component component)
+        {
+            components.Add(component);
+            return this;
+        }
+
+        public void render()
+        {
+            Console.SetCursorPosition(0, 0);
+            components.ForEach(component => {
+                foreach(String componentLine in component.getRenderedLines())
+                {
+                    String line = paddingLeft + componentLine;
+                    if (component.getClearTheRestOfEachLine())
+                    {
+                        Console.Write("{0}{1}", line, new string(' ', Console.WindowWidth - line.Length % Console.WindowWidth));
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0}", line);
+                    }
+                }
+            });
+        }
+    }
+
+    interface Component
+    {
+        bool getClearTheRestOfEachLine();
+        String[] getRenderedLines();
+    }
+
+    class MessageComponent : Component
+    {
+        private String[] messages;
+        private bool clearTheRestOfEachLine = true;
+
+        public MessageComponent() { }
+        public MessageComponent(params String[] messages)
+        {
+            setMessages(messages);
+        }
+        public void setMessages(params String[] messages)
+        {
+            this.messages = messages;
+        }
+        public void setClearTheRestOfEachLine(bool clear)
+        {
+            clearTheRestOfEachLine = clear;
+        }
+        public String[] getRenderedLines()
+        {
+            return messages;
+        }
+        public bool getClearTheRestOfEachLine()
+        {
+            return clearTheRestOfEachLine;
+        }
+    }
+
+    class MultipleChoiceComponent : Component
+    {
+        private String[] choices;
+        private int selectedIndex;
+
+        public void setSelectedIndex(int index)
+        {
+            if(index < 0)
+            {
+                selectedIndex = 0;
+                return;
+            }
+            if (index >= choices.Length)
+            {
+                selectedIndex = choices.Length - 1;
+                return;
+            }
+            selectedIndex = index;
+        }
+
+        public int getSelectedIndex() { return selectedIndex; }
+
+        public MultipleChoiceComponent(params String[] choices)
+        {
+            if (choices.Length == 0) throw new Exception("At least one option should be provided");
+            this.choices = choices;
+        }
+
+        public String[] getRenderedLines()
+        {
+            String[] lines = new String[choices.Length];
+            for (int i = 0; i < choices.Length; i++)
+            {
+                lines[i] = ""+ ((i == selectedIndex)? "->": "  ") + choices[i];
+            }
+            return lines;
+        }
+
+        public bool getClearTheRestOfEachLine()
+        {
+            return true;
+        }
+    }
+
+    class PasswordComponent : Component
+    {
+        private String password;
+        private List<String> messages;
+
+        public PasswordComponent(String password)
+        {
+            this.password = password;
+        }
+
+        public void setMessages(params String[] messages)
+        {
+            this.messages = messages.ToList();
+        }
+
+        public String[] getRenderedLines()
+        {
+            List<String> lines = messages.GetRange(0, messages.Count);
+            lines.Add("");
+            lines.Add("Please Enter the Admin Password:");
+            return lines.ToArray();
+        }
+
+        public bool getClearTheRestOfEachLine()
+        {
+            return true;
+        }
+    }
+
+    class Movie
+    {
+        private String name;
+        private String ageLimit;
+
+        public String getName()
+        {
+            return name;
+        }
+        public String getAgeLimit()
+        {
+            return ageLimit;
+        }
+
+        public Movie(String name, String ageLimit)
+        {
+            this.name = name;
+            this.ageLimit = ageLimit;
+        }
+    }
+
+    public delegate Page Page();
+
+    class Program
+    {
+        const String PASSWORD = "1234";
+
+        static Renderer renderer = new Renderer();
+        static MessageComponent bannderComponent = new MessageComponent(
+                "",
+                "███╗   ███╗ ██████╗ ██╗   ██╗██╗███████╗██████╗ ██╗     ███████╗██╗  ██╗",
+                "████╗ ████║██╔═══██╗██║   ██║██║██╔════╝██╔══██╗██║     ██╔════╝╚██╗██╔╝",
+                "██╔████╔██║██║   ██║██║   ██║██║█████╗  ██████╔╝██║     █████╗   ╚███╔╝ ",
+                "██║╚██╔╝██║██║   ██║╚██╗ ██╔╝██║██╔══╝  ██╔═══╝ ██║     ██╔══╝   ██╔██╗ ",
+                "██║ ╚═╝ ██║╚██████╔╝ ╚████╔╝ ██║███████╗██║     ███████╗███████╗██╔╝ ██╗",
+                "╚═╝     ╚═╝ ╚═════╝   ╚═══╝  ╚═╝╚══════╝╚═╝     ╚══════╝╚══════╝╚═╝  ╚═╝",
+                "",
+                "");
+
+        static Movie[] movies = new Movie[0];
+
+        static void Main(string[] args)
+        {
+            Page nextPage = new Page(home);
+            while(nextPage != null)
+            {
+                nextPage = nextPage();
+            }
+        }
+
+        static Page home()
+        {
+            MultipleChoiceComponent mainMenuComponent = new MultipleChoiceComponent("Administrator", "Guests");
+            renderer
+                .clear()
+                .addComponent(bannderComponent)
+                .addComponent(mainMenuComponent);
+            while (true)
+            {
+                renderer.render();
+                ConsoleKey key = Console.ReadKey().Key;
+                clearPreviousLines(0);
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        {
+                            mainMenuComponent.setSelectedIndex(mainMenuComponent.getSelectedIndex() - 1);
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            mainMenuComponent.setSelectedIndex(mainMenuComponent.getSelectedIndex() + 1);
+                            break;
+                        }
+                    case ConsoleKey.Enter:
+                        {
+                            return (mainMenuComponent.getSelectedIndex() == 0)? new Page(password) : new Page(guest);
+                        }
+                }
+            }
+        }
+        static Page password()
+        {
+            const String BACK = "B";
+            int attemptLeft = 5;
+
+            PasswordComponent passwordComponent = new PasswordComponent(PASSWORD);
+            passwordComponent.setMessages("", "");
+            renderer
+                .clear()
+                .addComponent(bannderComponent)
+                .addComponent(passwordComponent);
+            while (true)
+            {
+                renderer.render();
+                Console.Write(Renderer.paddingLeft);
+                String input = Console.ReadLine();
+                switch (input)
+                {
+                    case PASSWORD:
+                        {
+                            return new Page(administrator);
+                        }
+                    case BACK:
+                        {
+                            return new Page(home);
+                        }
+                    default:
+                        {
+                            attemptLeft--;
+                            if(attemptLeft <= 0) return new Page(home);
+                            clearPreviousLines(1);
+                            passwordComponent.setMessages(
+                                "You entered an Invalid password",
+                                "You have "+ attemptLeft + " more attempts to enter the correct password OR Press "+ BACK + " to go back to the previous screen.");
+                            break;
+                        }
+                }
+            }
+        }
+        static Page administrator()
+        {
+            Console.Clear();
+            Console.WriteLine("\n{0}Welecome MobiePlex Administrator\n", Renderer.paddingLeft);
+
+            int movieCount;
+            String movieCountInput;
+            do
+            {
+                Console.Write(Renderer.paddingLeft);
+                Console.Write("How many movies are playing today?: ");
+                movieCountInput = Console.ReadLine();
+                //TODO: handle input number over maxium value
+                //TODO: show error message when necessary
+            } while (!int.TryParse(movieCountInput, out movieCount));
+            Console.WriteLine("");
+            Program.movies = new Movie[movieCount];
+            for(int i = 0; i < movieCount; i++)
+            {
+                Console.Write(Renderer.paddingLeft);
+                Console.Write("Please Enter the #" + (i + 1) + " Movie's Name: ");
+                String name = Console.ReadLine();
+                Console.Write(Renderer.paddingLeft);
+                Console.Write("Please Enter the Age Limit or Rating for the #" + (i + 1) + " Movie: "); //TODO: handle the spelling of #i
+                String ageLimit = Console.ReadLine(); //TODO: handle wrong inputs
+                movies[i] = new Movie(name, ageLimit);
+            }
+
+            renderer
+                .clear()
+                .addComponent(bannderComponent);
+            renderer.render();
+
+            for (int i = 0; i < movies.Length; i++)
+            {
+                Console.WriteLine("       {0}. {1} {{{2}}}", i + 1, movies[i].getName(), movies[i].getAgeLimit());
+            }
+            while (true)
+            {
+                Console.WriteLine("\nYour Movies Playing Today Are Listed Above. Are you satisfied? (Y/N)");
+                String confirmed = Console.ReadLine();
+                switch (confirmed)
+                {
+                    case "Y":
+                    case "y":
+                        {
+                            return new Page(home);
+                        }
+                    case "N":
+                    case "n":
+                        {
+                            return new Page(administrator);
+                        }
+                    default:
+                        {
+                            clearPreviousLines(3);
+                            break;
+                        }
+                }
+            }
+        }
+        static Page guest()
+        {
+            if(Program.movies.Length == 0)
+            {
+                renderer
+                    .clear()
+                    .addComponent(bannderComponent)
+                    .addComponent(new MessageComponent("No moive available. Press any key to go back to the Start Page."));
+                renderer.render();
+                Console.ReadKey();
+                return new Page(home);
+            }
+
+            MessageComponent movieMenuTitleComponemt = new MessageComponent(
+                "There are " + Program.movies.Length + " movies playing today. Please choose from the following movies:",
+                "Which Movie Would You Like to Watch:"
+            );
+            String[] options = new String[Program.movies.Length];
+            for (int i = 0; i < Program.movies.Length; i++)
+            {
+                options[i] = (i + 1) + ". " + Program.movies[i].getName() + "{" + Program.movies[i].getAgeLimit() + "}";
+            }
+            MultipleChoiceComponent movieMenuComponent = new MultipleChoiceComponent(options);
+            renderer
+                .clear()
+                .addComponent(bannderComponent)
+                .addComponent(movieMenuTitleComponemt)
+                .addComponent(movieMenuComponent);
+
+            int selectedIndex = -1;
+            while (selectedIndex < 0)
+            {
+                renderer.render();
+                ConsoleKey key = Console.ReadKey().Key;
+                clearPreviousLines(0);
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        {
+                            movieMenuComponent.setSelectedIndex(movieMenuComponent.getSelectedIndex() - 1);
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            movieMenuComponent.setSelectedIndex(movieMenuComponent.getSelectedIndex() + 1);
+                            break;
+                        }
+                    case ConsoleKey.Enter:
+                        {
+                            selectedIndex = movieMenuComponent.getSelectedIndex();
+                            break;
+                        }
+                }
+            }
+            Console.WriteLine("");
+            Console.Write(Renderer.paddingLeft);
+            Console.Write("Please Enter Your Age for Verification: ");
+            String ageInput = Console.ReadLine();
+            int userAge;
+            if (int.TryParse(ageInput, out userAge))
+            {
+                int minimumAgeForTheMovie = 0; //TODO: get the correct minimum age from Program.movies[i].getAgeLimit()
+                if(userAge < minimumAgeForTheMovie)
+                {
+                    Console.Write(Renderer.paddingLeft);
+                    Console.WriteLine("Invalid input. Press any key to start over.");
+                    Console.ReadKey();
+                    return new Page(guest);
+                }
+            }
+            else
+            {
+                Console.Write(Renderer.paddingLeft);
+                Console.WriteLine("Invalid input. Press any key to start over.");
+                Console.ReadKey();
+                return new Page(guest);
+            }
+
+
+            MessageComponent ageInputPlaceholderComponent = new MessageComponent("", "");
+            ageInputPlaceholderComponent.setClearTheRestOfEachLine(false);
+            MessageComponent enjoyTheMovieComponent = new MessageComponent("", "Enjoy the Movie!", "");
+            MultipleChoiceComponent goBackPageMenuComponent = new MultipleChoiceComponent("go back to the Guest Main Menu.", "go back to the Start Page.");
+            renderer
+                .addComponent(ageInputPlaceholderComponent)
+                .addComponent(enjoyTheMovieComponent)
+                .addComponent(goBackPageMenuComponent);
+            while (true)
+            {
+                renderer.render();
+                ConsoleKey key = Console.ReadKey().Key;
+                clearPreviousLines(0);
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        {
+                            goBackPageMenuComponent.setSelectedIndex(goBackPageMenuComponent.getSelectedIndex() - 1);
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            goBackPageMenuComponent.setSelectedIndex(goBackPageMenuComponent.getSelectedIndex() + 1);
+                            break;
+                        }
+                    case ConsoleKey.Enter:
+                        {
+                            return (goBackPageMenuComponent.getSelectedIndex() == 0) ? new Page(guest) : new Page(home);
+                        }
+                }
+            }
+
+        }
+
+        static Page end()
+        {
+            return null;
+        }
+
+        private static void clearPreviousLines(int lineCount)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - lineCount);
+            for(int i = 0; i <= lineCount; i++)
+            {
+                Console.Write(new string(' ', Console.WindowWidth));
+            }
+            Console.SetCursorPosition(0, Console.CursorTop - lineCount - 1);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# dot-net-final-project
